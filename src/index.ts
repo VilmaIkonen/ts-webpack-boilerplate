1729f0e85c4fcd59080b8fe9e0a7c5c3437a6782
VilmaIkonen <vilma.r.ikonen@gmail.com>
2021-01-28 11:20:50 +0200

    Hoang's code added

---
+++ src/index.ts
@@ -0,0 +1,39 @@
+import { Cat, Dog, QuestionableAnimalThatAlsoPurr } from './interfaces/pet.interfaces'
+import { cats, dogs } from './pet.data'
+
+const animal: QuestionableAnimalThatAlsoPurr = {
+    name: "Susan",
+    age: 10,
+    weight: 2,
+    height: 3,
+    microChipped: false,
+    spayedOrNeutered: true,
+    breed: "chepard",
+    purr: () => 'grao'
+};
+
+const pets: Array<Cat | Dog> = [...dogs, ...cats];
+// pets.push(...dogs, ...cats);
+
+pets.push(animal as Cat);
+
+console.log(pets);
+
+const adopt = (subject: Dog): void => {
+    console.log(`We have adopted a dog name ${subject.name} and is ${subject.age} years old`)
+}
+
+dogs.forEach(adopt)
+
+// const isDog = (pet: Cat | Dog): boolean => Boolean((pet as Dog).bark)
+// const isCat = (pet: Cat | Dog): boolean => Boolean((pet as Cat).purr)
+
+const isDog = (pet: Cat | Dog): boolean => Boolean((pet as Dog).type === 'dog')
+const isCat = (pet: Cat | Dog): boolean => Boolean((pet as Cat).type === 'cat')
+
+ 
+
+pets.forEach((pet) => {
+    if (isDog(pet)) console.log('Dog!')
+    else console.log('Cat!')
+})
---
+++ src/interfaces/pet.interfaces.ts
@@ -0,0 +1,24 @@
+interface Pet {
+  name: string,
+  age: number,
+  height?: number,
+  weight: number,
+  microChipped: boolean,
+  breed: string,
+  spayedOrNeutered: boolean
+}
+
+export interface Dog extends Pet {
+  type: string,
+  sporty: boolean,
+  bark: () => void
+}
+
+export interface Cat extends Pet {
+  type: string,
+  purr: () => void
+}
+
+export interface QuestionableAnimalThatAlsoPurr extends Pet {
+  purr: () => string
+}
